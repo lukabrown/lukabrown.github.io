9bdbde32db0b097fc5b1c021500750fbc908e057
lukabrown <117334114+lukabrown@users.noreply.github.com>
2026-01-13 22:50:45 -0600

    adds play button

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("playButton").addEventListener("click", function() {
+    // Hide splash
+    document.getElementById("splash").style.display = "none";
+    // Show main content
+    document.getElementById("mainContent").style.display = "block";
+});
