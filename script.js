b471fda8d3b5429ba1aeda3afca483f8f33be51e
lukabrown <117334114+lukabrown@users.noreply.github.com>
2026-01-14 23:29:56 -0600

    reorganization of style and script files

--- script.js
+++ script.js
@@ -1,6 +1,39 @@
-document.getElementById("playButton").addEventListener("click", function() {
-    // Hide splash
-    document.getElementById("splash").style.display = "none";
-    // Show main content
-    document.getElementById("mainContent").style.display = "block";
+function addEventListenersToProjects() {
+    const projects = document.querySelectorAll(".project");
+
+    projects.forEach(project => {
+        const projectImages = project.querySelector(".project-images");
+        if (!projectImages) return; // skip if missing
+
+        project.addEventListener("mouseover", () => {
+            projectImages.classList.add("project-images-visible");
+        });
+
+        project.addEventListener("mouseout", () => {
+            projectImages.classList.remove("project-images-visible");
+        });
+    });
+}
+
+function checkWindowSizeAndExecute() {
+    if (window.innerWidth >= 768) {
+        addEventListenersToProjects();
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const splash = document.querySelector(".splash");
+    const playButton = document.querySelector(".playButton");
+    const projectPage = document.querySelector(".projects");
+
+    if (projectPage) {
+        checkWindowSizeAndExecute();
+        window.addEventListener('resize', checkWindowSizeAndExecute);
+    };
+
+    if (splash && playButton) {
+        document.querySelector(".playButton").addEventListener("click", () => {
+        document.querySelector(".splash").style.display = "none";
+        });
+    };
 });
